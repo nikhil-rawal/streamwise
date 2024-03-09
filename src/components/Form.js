1a25cf365c29be8ad99c50a35cd8816d5e0b6bef
Nikhil Rawal <rawal.nikhil29@gmail.com>
2024-03-09 10:06:50 -0500

    working - start

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <form className="text-white absolute px-8 py-4 bg-black w-4/12 left-1/2 -translate-x-1/2 translate-y-1/2 flex flex-col justify-center content-center items-center opacity-90">
-      <h1 className=" font-semibold  self-start p-2 m-2 text-3xl">Sign In</h1>
+      <h1 className=" font-semibold  self-start p-2 m-2 text-3xl">
+        {isSignInForm ? "Sign In" : "Sign Up"}
+      </h1>
+      {!isSignInForm && (
+        <input
+          type="text"
+          placeholder="Full Name"
+          className={`p-2 m-2 w-full rounded-md bg-[#333] hover:bg-[#333333e5] leading-8`}
+        />
+      )}
       <input
         type="text"
         placeholder="Email Address"
@@ -15,12 +30,18 @@ const Form = () => {
         className="p-2 m-2 w-full rounded-md bg-[#333] hover:bg-[#333333e5] leading-8"
       />
       <button className="bg-[#E50914] w-full rounded-md p-1 m-2 leading-8 hover:bg-[#e50914e8] my-6">
-        Sign In
+        {isSignInForm ? "Sign In" : "Sign Up"}
       </button>
       <br />
-      <p className="font-light self-start">
+      <p className={`font-light self-start`}>
         New to Streamwise?{" "}
-        <span className="underline font-bold ">Sign up now</span>.
+        <span
+          className="underline font-bold cursor-pointer"
+          onClick={toggleSignInForm}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </span>
+        .
       </p>
     </form>
   );
