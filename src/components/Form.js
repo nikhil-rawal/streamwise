4999e7b7488f5ca18f65c42a886b9c3e6e1618c4
Nikhil Rawal <rawal.nikhil29@gmail.com>
2024-03-21 10:15:59 -0400

    working on form validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { validateData } from "../utils/validate";
 
 const Form = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -7,9 +8,18 @@ const Form = () => {
     setIsSignInForm(!isSignInForm);
   };
 
+  const handleButtonClick = () => {
+    validateData(email.current.value, password.current.value);
+  };
+
+  const email = useRef(null);
+  const password = useRef(null);
+
   return (
-    <form className="text-white absolute px-8 py-4 bg-black w-[36%] mx-auto my-auto flex flex-col justify-center content-center items-center opacity-90 translate-x-96 translate-y-36">
-      {/* left-1/2 -translate-x-1/2 translate-y-1/2 */}
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="text-white absolute px-8 py-4 bg-black w-[36%] mx-auto my-auto flex flex-col justify-center content-center items-center opacity-90 translate-x-96 translate-y-36"
+    >
       <h1 className=" font-semibold  self-start p-2 m-2 text-3xl">
         {isSignInForm ? "Sign In" : "Sign Up"}
       </h1>
@@ -21,21 +31,24 @@ const Form = () => {
         />
       )}
       <input
+        ref={email}
         type="text"
         placeholder="Email Address"
         className="p-2 m-2 w-full rounded-md bg-[#333] hover:bg-[#333333e5] leading-8"
       />
       <input
+        ref={password}
         type="text"
         placeholder="Password"
         className="p-2 m-2 w-full rounded-md bg-[#333] hover:bg-[#333333e5] leading-8"
       />
-      <button className="bg-[#E50914] w-full rounded-md p-1 m-2 leading-8 hover:bg-[#e50914e8] my-6">
+      <button
+        className="bg-[#E50914] w-full rounded-md p-1 m-2 leading-8 hover:bg-[#e50914e8] my-6"
+        onClick={handleButtonClick}
+      >
         {isSignInForm ? "Sign In" : "Sign Up"}
       </button>
       <br />
-      {/* {isSignInForm ? "Sign Up" : "Sign In"} */}
-
       {isSignInForm ? (
         <p className={`font-light self-start`}>
           New to Streamwise?{" "}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="w-full text-left px-10 py-8 logo-text absolute z-10 text-[#E50914] font-bold text-4xl tracking-widest scale-y-125 backdrop-blur-[1px] subpixel-antialiased ">
-      STREAMWISE
+    <div className="w-full text-left px-10 py-8 logo-text absolute z-10 text-[#E50914] font-bold text-4xl tracking-widest scale-y-125 backdrop-blur-[.5px] subpixel-antialiased cursor-pointer">
+      <Link to="/">STREAMWISE</Link>
     </div>
   );
 };
